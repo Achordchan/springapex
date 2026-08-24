1b409c78b75fba7c9a51ce6883ef77ef65e9c5c3
Achord <a1234@Achord-MacBook-Pro-M1-Max.local>
2026-08-25 00:19:04 +0800

    chore: update production Turnstile site key

--- wp-content/themes/springapex/functions.php
+++ wp-content/themes/springapex/functions.php
@@ -9,7 +9,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('SPRINGAPEX_VERSION', '2.9.96');
+define('SPRINGAPEX_VERSION', '2.9.97');
 define('SPRINGAPEX_DIR', get_template_directory());
 define('SPRINGAPEX_URI', get_template_directory_uri());
 
